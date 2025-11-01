d0a8dd61dad4b9569df4c45ce5f99e06c98e92fe
STU6266 <skemendics@byupathway.edu>
2025-11-01 22:06:10 +0100

    modified:   database/assignment2.sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -3,7 +3,7 @@
 -- 1. Insert the following new record to the account table
 -- Tony, Stark, tony@starkent.com, Iam1ronM@n
 INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n'),
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
 
 -- 2 Modify the Tony Stark record to change the account_type to "Admin".
